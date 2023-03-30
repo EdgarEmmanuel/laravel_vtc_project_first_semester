91ccde27a42212f6c0c86a1d6bd0cdac5c181f0a
Edgar <mmnledgar@gmail.com>
2023-03-30 16:59:39 +0000

    handling chauffeur

--- app/Http/Controllers/AuthDriverController.php
+++ app/Http/Controllers/AuthDriverController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chauffeur;
+use DateTimeImmutable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 
 class AuthDriverController extends Controller
 {
@@ -20,7 +24,48 @@ class AuthDriverController extends Controller
     {
        $credentials = $request->only('email', 'password');
 
-       dd($credentials);
+       $chauffeur = Chauffeur::where("email", $credentials["email"])->first();
+
+       if($chauffeur == null){
+        return response()->json([
+            'success'=> false,
+            'code' => 403,
+            'error' => [
+                "message" => "aucun chaufeur avec cet email ou ce mot de passe"
+            ]
+        ]);
+       } else {
+        if(Hash::check($credentials["password"], $chauffeur->password)){
+            //dd($chauffeur);
+            $secretKey  = 'bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew=';
+            $issuedAt   = new DateTimeImmutable();
+            $expire     = $issuedAt->modify('+40 minutes')->getTimestamp();      // Add 60 seconds
+            $serverName = "your.domain.name";
+            $username   = "username";                                           // Retrieved from filtered POST data
+
+            $data = [
+                'iat'  => $issuedAt->getTimestamp(),         // Issued at: time when the token was generated
+                'iss'  => $serverName,                       // Issuer
+                'nbf'  => $issuedAt->getTimestamp(),         // Not before
+                'exp'  => $expire,                           // Expire
+                'chauffeur' => $chauffeur,                     // User name
+            ];
+            return response()->json([
+                'success' => true,
+                'code' => 200,
+                'error' => null,
+                'token' => JWT::encode($data, env("JWT_SECRET"), 'HS256')
+            ]);
+        } else {
+            return response()->json([
+                'success'=> false,
+                'code' => 403,
+                'error' => [
+                    "message" => "aucun chaufeur avec cet email ou ce mot de passe"
+                ]
+            ]);
+        }
+       }
     }
 
 
